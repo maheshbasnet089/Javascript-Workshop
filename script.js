cc528082cabdba4434986e5c315f7354f65c15da
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-03 19:08:38 +0545

    learn about variable, array, object

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
-console.log("bye world")
+// console.log("bye world")
 
-// node --watch script.js
+
+const glass = "paani"
+// const age = 22 
+// const isNepali = true
+
+
+const gamalaa = "fulll" 
+
+const friend1 = "Pranjal"
+const friend2 = "Aashish"
+const friend3 = "Anup"
+
+
+const friends = ["Pranjal","Aaashish","Anup"]
+
+const shoe1 = "Addidas"
+const shoe2 = "Puma"
+const shoe3 = "Umbro"
+
+
+
+
+
+
+
+
+const shoes = ['Addidas','Puma','Umbro','Nike','Zoom']
+
+// console.log(shoes[0])
+// console.log(shoes[3])
+
+// properties and methods 
+// console.log(shoes.length)
+
+
+// shoes.push('fasttrack')
+// shoes.pop()
+
+// shoes.unshift('fasttrack')
+// shoes.shift()
+// shoes[2] = "fasttrack"
+// console.log(shoes)
+shoes.splice(2,2,'fasttrack')
+console.log(shoes)
+
+// splice
+
+
+
+
+
+
+
+
+
+
+
+
+const name = "Manish Basnet" 
+const age = 22 
+const address = "Itahari"
+const isNepali = true 
+
+const me = {
+    name : "Manish Basnet", 
+    age : 22, 
+    address : "Itahari", 
+    isNepali : true 
+}
+
